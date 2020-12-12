45f8fc3425ef86f372ef8ccf9f8289c85d8789d1
shenys17 <shenysh.17@sem.tsinghua.edu.cn>
2020-12-12 12:48:38 +0800

    调整sliding window等参数，误码率稍微降低

--- test/test_update.m
+++ test/test_update.m
@@ -24,8 +24,8 @@ end
 disp(t);
 len = length(message);
 data_bit = message(t+24000:len); 
-figure(2);
-plot(data_bit);
+%figure(2);
+%plot(data_bit);
 
 f = 6000;%目标频率
 [n,~] = size(data_bit);%获取数据的长度值
@@ -42,21 +42,21 @@ for i= 1:1:n-window
 end
 
 % 滑动平均（均值滤波）
-sliding_window = 5;
+sliding_window = 10;
 impulse_fft_tmp = impulse_fft_6k;
 for i = 1+sliding_window:1:n-sliding_window
     impulse_fft_tmp(i)=mean(impulse_fft_6k(i-sliding_window:i+sliding_window));
 end
 impulse_fft_6k = impulse_fft_tmp;
-figure(3)
-plot(impulse_fft_6k);
+%figure(3)
+%plot(impulse_fft_6k);
 
 % peak
 position_impulse=[];%用于存储峰值的index
-half_window = 800;
+half_window = 600;
 for i= half_window+1:1:n-half_window
     %进行峰值判断
-    if impulse_fft_6k(i)>60 && impulse_fft_6k(i)==max(impulse_fft_6k(i-half_window:i+half_window))
+    if impulse_fft_6k(i)> 40 && impulse_fft_6k(i)==max(impulse_fft_6k(i-half_window:i+half_window))
         position_impulse=[position_impulse,i];
     end
 end
@@ -79,7 +79,6 @@ real_message_bin = message_bin(real_message_start:len);
 len = length(real_message_bin);
 real_message_vec = real_message_bin;
 
-
 real_message_bin = real_message_bin';
 
 % 结果矩阵，每一行代表一个包，第一列为包编号, 每行50列，不足的列用-1补全（方便输出到xls）
@@ -101,17 +100,29 @@ while (curr_ptr+8<len)
     curr_ptr = curr_ptr+curr_len;
     %将当前行补全
     curr_row_len = length(curr_row);
+    if(curr_row_len>50)
+        break;
+    end
     if(curr_row_len < 50)
         for i=1:1:50-curr_row_len
             curr_row = [curr_row, -1];
         end
     end
+    disp(package_index);
+    disp(length(curr_row));
     %将当前行加入结果矩阵
     result = [result;curr_row];
     %寻找下一段前导码
     find_next = 0;
     while(curr_ptr+19 <= len)
-        if isequal(bin_pre, real_message_bin(curr_ptr:curr_ptr+19))
+        samecount = 0
+        for p = 1:1:20
+            if (bin_pre(p) == real_message_bin(curr_ptr+p-1))
+                samecount = samecount +1;
+            end
+        end
+        if samecount > 18
+        %if isequal(bin_pre, real_message_bin(curr_ptr:curr_ptr+19))
             find_next = 1;
             break;
         end
@@ -122,7 +133,6 @@ while (curr_ptr+8<len)
     end
     curr_ptr = curr_ptr+20;
 end
-
 
 xlswrite('result.xls', result);
 
