77b8c19d5bd79c6dcfb8198b981681dbe5aaa589
Leonidas9826 <49238205+Leonidas9826@users.noreply.github.com>
2020-12-24 16:56:20 +0800

    修复distance_sender.m中调用de2bi函数的bug

--- scripts/distance_sender.m
+++ scripts/distance_sender.m
@@ -1,6 +1,7 @@
 %This script is used to send signal to measure distance
 %Version 1.2. created on 2020.11.17, updated on 2020.11.21, author: swy
 time_vector = clock;
+disp(time_vector);
 h = time_vector(4);
 m = time_vector(5);
 s = time_vector(6);
@@ -9,11 +10,13 @@ h = h*100;
 m = m*100;
 s = s*1000;
 preamble = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
-data_vector = [h, m, s];
+temp = [h, m, s];
+data_vector = cast(temp,'uint32');
+
 data_bin = de2bi(data_vector, 16, 'left-msb');
 disp(data_bin);
 data_bits = reshape(double(data_bin).',48,1).';
 send_bits = [preamble, data_bits];
 FSK_code = bits2FSK(send_bits);
 FSK2WAV(FSK_code);
-% sound(FSK_code, 10000);
+sound(FSK_code, 10000);
